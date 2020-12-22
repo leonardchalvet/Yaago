509d8f26944704fe20a7d4c3da3e5e28425f1210
Léonard Chalvet <leonard@agence-me.com>
2020-12-22 11:10:15 +0100

    Add - Front - Header

--- Front/common-header.php
+++ Front/common-header.php
@@ -1,1 +1,65 @@
-<header></header>
+<header>
+	<div class="head">
+		<div class="head-mobile">
+			
+			<div class="burger">
+				<div class="line"></div>
+				<div class="line"></div>
+				<div class="line"></div>
+			</div>
+			<a href="1a-home.php" class="logo">
+				<img src="img/common/logo.svg" alt="">
+			</a>
+			<a href="" class="btn">
+				<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+					<use xlink:href="img/common/arrow-1.svg#content"></use>
+				</svg>
+			</a>
+		</div>
+		<div class="wrapper">
+			<div class="container-link">
+				<ul>
+					<li>
+						<a href="" class="link">
+							<span class="title">Offres</span>
+						</a>
+					</li>
+					<li>
+						<a href="" class="link">
+							<span class="title">Blog</span>
+						</a>
+					</li>
+					<li>
+						<a href="" class="link">
+							<span class="title">A Propos</span>
+						</a>
+					</li>
+					<li>
+						<a href="" class="link">
+							<span class="title">Solutions</span>
+						</a>
+					</li>
+					<li>
+						<a href="" class="link">
+							<span class="title">Faq</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<a href="" class="logo">
+				<img src="img/common/logo.svg" alt="">
+			</a>
+			<div class="container-action">
+				<a href="" class="login">
+					<span class="link-text">Se connecter</span>
+				</a>
+				<a href="" class="btn">
+					<span class="btn-text">Testez</span>
+					<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+						<use xlink:href="img/common/arrow-1.svg#content"></use>
+					</svg>
+				</a>
+			</div>
+		</div>
+	</div>
+</header>
--- Front/script/js/common.js
+++ Front/script/js/common.js
@@ -1,7 +1,22 @@
 // @codekit-prepend 'jQuery.3.3.1.js'
-
+$window = $(window);
 
 /* FOOTER */
 $('footer .foot .container-lg .lg .text').click(function(){
 	$('footer .foot .container-lg .dropdown').toggleClass('style-show');
+})
+
+
+/* HEADER */
+$window.scroll(function() {
+	if ( $window.scrollTop() >= 150 ) {
+        $('header').addClass('style-scroll');
+    } else {
+    	$('header').removeClass('style-scroll');
+    };
+});
+
+$('header .head .head-mobile .burger').click(function(){
+	$('header').toggleClass('style-open');
+	$('body').toggleClass('no-scroll');
 })
