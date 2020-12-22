627a56ea3651ef799aea8ad56b42f0cac5a3017c
Léonard Chalvet <leonard@agence-me.com>
2020-12-22 10:28:44 +0100

    Add - Front - Footer

--- Front/common-footer.php
+++ Front/common-footer.php
@@ -1,1 +1,99 @@
-<footer></footer>
+<footer>
+	
+	<div class="content">
+		<div class="wrapper">
+			<div class="container-logo">
+				<a href="" class="logo">
+					<img src="img/common/logo.svg" alt="">
+				</a>
+				<p>
+					Les propriétaires qui font la différence. 
+				</p>
+				<div class="container-rs">
+					<a href="">
+						<img src="img/footer/icn-facebook.svg" alt="">
+					</a>
+					<a href="">
+						<img src="img/footer/icn-linkedin.svg" alt="">
+					</a>
+					<a href="">
+						<img src="img/footer/icn-instagram.svg" alt="">
+					</a>
+				</div>
+			</div>
+			<div class="container-newsletter">
+				<h2>Newsletter</h2>
+				<div class="input">
+					<input type="text" placeholder="Votre adresse mail">
+					<div class="send">
+						<img src="img/footer/icn-send.svg" alt="">
+					</div>
+				</div>
+			</div>
+
+			<div class="container-link">
+				<h2>Liens</h2>
+				<ul>
+					<li>
+						<a href="">Home</a>
+					</li>
+					<li>
+						<a href="">Faq</a>
+					</li>
+					<li>
+						<a href="">Offres</a>
+					</li>
+					
+					<li>
+						<a href="">Solutions</a>
+					</li>
+					<li>
+						<a href="">Blog</a>
+					</li>
+					<li>
+						<a href="">Partenaires</a>
+					</li>
+					<li>
+						<a href="">A Propos</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="container-sponsors">
+				<h2>Ils nous soutiennent</h2>
+				<div class="container-img">
+					<img src="img/footer/logo-vca.png" alt="">
+					<img src="img/footer/logo-normandie.png" alt="">
+					<img src="img/footer/logo-ft.png" alt="">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="foot">
+		<div class="wrapper">
+			<div class="container-lg">
+				<div class="lg">
+					<img class="img-lg" src="img/footer/icn-earth.svg" alt="">
+					<div class="text">
+						French
+					</div>
+					<img class="arrow" src="img/footer/icn-arrow.svg" alt="">
+				</div>
+				<div class="dropdown">
+					<div class="el">French</div>
+					<div class="el">English</div>
+				</div>
+			</div>
+
+			<p class="cpr">
+				©2020 Yaago. All rights Reserved
+			</p>
+
+			<p class="pld">
+				Privacy & Legal Documentation
+			</p>
+		</div>
+	</div>
+
+</footer>
--- Front/script/js/common.js
+++ Front/script/js/common.js
@@ -1,1 +1,7 @@
 // @codekit-prepend 'jQuery.3.3.1.js'
+
+
+/* FOOTER */
+$('footer .foot .container-lg .lg .text').click(function(){
+	$('footer .foot .container-lg .dropdown').toggleClass('style-show');
+})
