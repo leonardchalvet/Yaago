5921559cb8afabfa4872c7d670d137fd4a103e32
Léonard Chalvet <leonard@agence-me.com>
2020-12-24 15:24:44 +0100

    Change - Front - disable zoom input all pages

--- Front/index.php
+++ Front/index.php
@@ -5,7 +5,7 @@
 
 		<meta name="description" content="" />
 		<meta http-equiv="content-type" content="text/html; charset=utf8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link rel="stylesheet" type="text/css" href="style/css/home.css">
 	</head>
 	
