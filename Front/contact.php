bacd623a3de8c0cb24a370b4e97804a52ef6bd50
Léonard Chalvet <leonard@agence-me.com>
2020-12-24 14:52:30 +0100

    Change - front - feedbacks

--- Front/contact.php
+++ Front/contact.php
@@ -7,7 +7,7 @@
 
 		<meta http-equiv="content-type" content="text/html; charset=utf8" />
 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
 		<link rel="stylesheet" type="text/css" href="style/css/contact.css">
 
