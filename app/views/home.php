584aea38caea63180fa9adc6676ff6e20eee32fd
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-01-04 14:44:56 +0100

    Change - Back - All pages - Add link favicon

--- app/views/home.php
+++ app/views/home.php
@@ -16,6 +16,15 @@ $document = $WPGLOBAL['document']->data;
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <link rel="stylesheet" type="text/css" href="/style/css/home.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#03224c">
+    <meta name="msapplication-TileColor" content="#03224c">
+    <meta name="theme-color" content="#ffffff">
+    
   </head>
   
   <body>
