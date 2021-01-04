0d4671e567b8f668f1e2314dd9684054e89b2182
Léonard Chalvet <leonard@agence-me.com>
2021-01-04 16:49:30 +0100

    Add - Front - Blog Details

---
+++ Front/blog-details.php
@@ -0,0 +1,273 @@
+<html>
+	<head>
+
+		<title>Blog Details</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="style/css/blog-details.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+
+			<section class="section-cover">
+
+				<div class="wrapper">
+					<div class="container-title">
+						<h1>
+							Article d’exemple
+						</h1>
+						<p>
+							Suivez les 3 étapes de créations de compte en utilisant votre adresse mail, google ou facebook. 
+						</p>
+					</div>
+					<div class="container-img">
+						<img src="img/img-test/background-1.jpg" alt="">
+					</div>
+				</div>
+
+			</section>
+
+
+			<section class="section-blog">
+				<div class="wrapper">
+					<div class="container-share">
+						<a href="">
+							<img src="img/blog-details/section-blog/icn-facebook.svg" alt="">
+						</a>
+						<a href="">
+							<img src="img/blog-details/section-blog/icn-twitter.svg" alt="">
+						</a>
+					</div>
+					<div class="container-blog">
+						<div class="container-date">
+							<p>
+								Plublished on March 11, 2020  -  Updated on March 23, 2020
+							</p>
+						</div>
+						<h2>Title H2</h2>
+						<h3>Title H3</h3>
+						<h4>Title H4</h4>
+						<p>
+							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam tenetur molestiae non incidunt pariatur rem sint maiores quasi dolorem. Voluptas placeat inventore <a href="">tempore</a> distinctio rerum aliquid, nostrum quis ducimus doloribus.
+						</p>
+						<ul>
+							<li>
+								Lorem ipsum dolor sit amet, consectetur adipiscing
+							</li>
+							<li>
+								Lorem ipsum dolor sit amet, consectetur adipiscing 
+							</li>
+							<li>
+								Lorem ipsum dolor sit amet, consectetur adipiscing 
+							</li>
+						</ul>
+						<ol>
+							<li>
+								Lorem ipsum dolor sit amet, consectetur adipiscing 
+							</li>
+							<li>
+								Lorem ipsum dolor sit amet, consectetur adipiscing 
+							</li>
+							<li>
+								Lorem ipsum dolor sit amet, consectetur adipiscing 
+							</li>
+						</ol>
+
+						<div class="style-black">
+							<h2>Title H2</h2>
+							<h3>Title H3</h3>
+							<h4>Title H4</h4>
+							<p>
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam tenetur molestiae non incidunt pariatur rem sint maiores quasi dolorem. Voluptas placeat inventore <a href="">tempore</a> distinctio rerum aliquid, nostrum quis ducimus doloribus.
+							</p>
+							<ul>
+								<li>
+									Lorem ipsum dolor sit amet, consectetur adipiscing
+								</li>
+								<li>
+									Lorem ipsum dolor sit amet, consectetur adipiscing 
+								</li>
+								<li>
+									Lorem ipsum dolor sit amet, consectetur adipiscing 
+								</li>
+							</ul>
+							<ol>
+								<li>
+									Lorem ipsum dolor sit amet, consectetur adipiscing 
+								</li>
+								<li>
+									Lorem ipsum dolor sit amet, consectetur adipiscing 
+								</li>
+								<li>
+									Lorem ipsum dolor sit amet, consectetur adipiscing 
+								</li>
+							</ol>
+						</div>
+						
+						<a class="btn-1" href="">
+							<span class="btn-text">Button 1</span>
+							<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+								<use xlink:href="img/common/arrow-1.svg#content"></use>
+							</svg>
+						</a>
+						<br>
+						<a class="btn-2" href="">
+							<span class="btn-text">Button 2</span>
+							<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+								<use xlink:href="img/common/arrow-1.svg#content"></use>
+							</svg>
+						</a>
+						<br>
+						<a class="btn-3" href="">
+							<span class="btn-text">Button 3</span>
+							<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+								<use xlink:href="img/common/arrow-1.svg#content"></use>
+							</svg>
+						</a>
+						<br>
+						<a href="" class="link">
+							<span class="link-text">Testez</span>
+						</a>
+
+						
+						<img src="img/img-test/background-1.jpg" alt="">
+
+						
+						<div class="container-quote">
+							<div class="icn-quote">
+								<img src="img/blog-details/section-blog/icn-quote.svg" alt="">
+							</div>
+							<q>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed mauris pulvinar aenean pretium. Feugiat turpis in nam aliquet urna aliquam. Ultrices netus bibendum nisl, nullam. Nunc ut quis sed purus faucibus sed. Justo, lectus sit egestas libero, odio vivamus tortor. Vivamus ipsum eget diam cras varius tellus. 
+							</q>
+						</div>
+
+						<div class="container-list">
+							<ul>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+								<li>Nam sem velit</li>
+							</ul>
+						</div>
+						
+						<div class="container-author">
+							<div class="container-date">
+								<p>
+									Plublished on March 11, 2020  -  Updated on March 23, 2020
+								</p>
+							</div>
+							<div class="author">
+								<div class="pp">
+									<img src="img/img-test/user.jpg" alt="">
+								</div>
+								<div class="text">
+									<div class="name">About Alice Orru</div>
+									<div class="desc">
+										Alice is a content writer with strong experience in international customer service and empathic communication. She is an active member of the WordPress community: she loves translating WordPress into Italian and speaking at WordCamps. You can follow @Alice_Ridice on Twitter.
+									</div>
+								</div>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+			</section>
+
+			<section class="section-articles">
+				<div class="wrapper">
+					<div class="container-title">
+						<h2>Derniers Articles</h2>
+					</div>
+					<div class="container-articles">
+						<a href="" class="article">
+							<img src="img/img-test/background-1.jpg" alt="" class="cover">
+							<h3>
+								Lorem ipsum dolor sit amet non consequentur Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorem similique dicta commodi, numquam aliquid laboriosam sed reprehenderit qui, iure suscipit ullam, nesciunt asperiores, id dignissimos fuga illum! Provident, cupiditate.
+							</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium id doloribus distinctio laudantium at aliquam itaque! Consectetur repellat dolore possimus neque cupiditate consequuntur ullam aliquam ut, atque tempore iure dolorum.							</p>
+							<span class="author">
+								<span class="pp">
+									<img src="img/img-test/user.jpg" alt="">
+								</span>
+								<span class="text">
+									<span class="name">Albert Flores</span>
+									<span class="date">
+										Plume Yaago depuis 2016
+									</span>
+								</span>
+							</span>
+						</a>
+						<a href="" class="article">
+							<img src="img/img-test/background-1.jpg" alt="" class="cover">
+							<h3>
+								Lorem ipsum dolor sit amet non consequentur Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorem similique dicta commodi, numquam aliquid laboriosam sed reprehenderit qui, iure suscipit ullam, nesciunt asperiores, id dignissimos fuga illum! Provident, cupiditate.
+							</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium id doloribus distinctio laudantium at aliquam itaque! Consectetur repellat dolore possimus neque cupiditate consequuntur ullam aliquam ut, atque tempore iure dolorum.							</p>
+							<span class="author">
+								<span class="pp">
+									<img src="img/img-test/user.jpg" alt="">
+								</span>
+								<span class="text">
+									<span class="name">Albert Flores</span>
+									<span class="date">
+										Plume Yaago depuis 2016
+									</span>
+								</span>
+							</span>
+						</a>
+						<a href="" class="article">
+							<img src="img/img-test/background-1.jpg" alt="" class="cover">
+							<h3>
+								Lorem ipsum dolor sit amet non consequentur Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam dolorem similique dicta commodi, numquam aliquid laboriosam sed reprehenderit qui, iure suscipit ullam, nesciunt asperiores, id dignissimos fuga illum! Provident, cupiditate.
+							</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium id doloribus distinctio laudantium at aliquam itaque! Consectetur repellat dolore possimus neque cupiditate consequuntur ullam aliquam ut, atque tempore iure dolorum.							</p>
+							<span class="author">
+								<span class="pp">
+									<img src="img/img-test/user.jpg" alt="">
+								</span>
+								<span class="text">
+									<span class="name">Albert Flores</span>
+									<span class="date">
+										Plume Yaago depuis 2016
+									</span>
+								</span>
+							</span>
+						</a>
+					</div>
+				</div>
+			</section>
+
+
+			<?php include('common-section-demo.php') ?>
+
+
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/blog-details-min.js"></script>
+	</body>
+</html>
