c8e2ed0663f2f6ca62cb2441137ccbbfea4f3bc7
Léonard Chalvet <leonard@agence-me.com>
2021-01-06 11:06:41 +0100

    Change - Front - Blog Details - Feedbacks interne

---
+++ Front/landing.php
@@ -0,0 +1,115 @@
+<html>
+	<head>
+
+		<title>Landing</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="style/css/landing.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+			<section class="section-cover">
+				<div class="wrapper">
+					<div class="container-illu">
+						<img src="img/landing/section-cover/illu.svg" alt="">
+					</div>
+					<div class="container-title">
+						<h1>
+							<span>Les propriétaires qui font la</span>
+							<span>différence</span>
+						</h1>
+						<div class="container-input">
+							<input type="text" placeholder="Entrez votre Email">
+							<a href="" class="btn">
+								<span class="btn-text">Je découvre </span>
+							</a>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-solutions">
+				<div class="wrapper">
+					<div class="container-title">
+						<h2>Nos <em>solutions</em></h2>
+						<p>
+							Tout ce dont vous avez besoin au même endroit !
+						</p>
+					</div>
+					<div class="container-el">
+						<div class="el">
+							<div class="illu">
+								<img src="img/landing/section-solutions/illu-1.png" alt="">
+							</div>
+							<h3>Centralisez vos annonces </h3>
+							<p>
+								Votre annonce est visible sur plusieurs plateformes comme Airbnb, Booking, et HomeAway ? Yaago vous permet de centraliser en un seul endroit la gestion de votre planning à travers un tableau de bord unique et une vue 360° sur vos propriétés.
+							</p>
+						</div>
+						<div class="el">
+							<div class="illu">
+								<img src="img/landing/section-solutions/illu-2.png" alt="">
+							</div>
+							<h3>Centralisez vos annonces </h3>
+							<p>
+								Votre annonce est visible sur plusieurs plateformes comme Airbnb, Booking, et HomeAway ? Yaago vous permet de centraliser en un seul endroit la gestion de votre planning à travers un tableau de bord unique et une vue 360° sur vos propriétés.
+							</p>
+						</div>
+						<div class="el">
+							<div class="illu">
+								<img src="img/landing/section-solutions/illu-3.png" alt="">
+							</div>
+							<h3>Centralisez vos annonces </h3>
+							<p>
+								Votre annonce est visible sur plusieurs plateformes comme Airbnb, Booking, et HomeAway ? Yaago vous permet de centraliser en un seul endroit la gestion de votre planning à travers un tableau de bord unique et une vue 360° sur vos propriétés.
+							</p>
+						</div>
+					</div>
+					<div class="container-btn">
+						<a class="btn" >
+							<div class="btn-text">En savoir plus</div>
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-tarif">
+				<div class="wrapper">
+					<div class="illu">
+						<img src="img/landing/section-tarif/illu-1.svg" alt="">
+					</div>
+					<div class="container-text">
+						<h2>
+							Enfin une tarification simple et <em>transparente</em>
+						</h2>
+						<p>
+							Nous vous proposons un tarif dégressif, sans frais d’installation et sans formation payante.
+						</p>
+						<a href="" class="btn">
+							<span class="btn-text">Testez</span>
+							<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+								<use xlink:href="img/common/arrow-1.svg#content"></use>
+							</svg>
+						</a>
+					</div>
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/landing-min.js"></script>
+	</body>
+</html>
