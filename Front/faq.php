6005bae4e06d991a0c85f9d92781d284296af763
Léonard Chalvet <leonard@agence-me.com>
2021-01-11 14:36:53 +0100

    Add - Front - FAQ

---
+++ Front/faq.php
@@ -0,0 +1,159 @@
+<html>
+	<head>
+
+		<title>Faq</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="style/css/faq.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+			<section class="section-title">
+				<div class="wrapper">
+					<div class="container-title">
+						<h1>
+							FAQ
+						</h1>
+						<p>
+							Besoin d’aide ? Une question ? On vous guide pas à pas 
+						</p>
+					</div>
+					<ul>
+						<li>
+							<a href="">All</a>
+						</li>
+						<li>
+							<a href="" class="style-active">Login / Register</a>
+						</li>
+						<li>
+							<a href="">Recommendations</a>
+						</li>
+						<li>
+							<a href="">General / Settings</a>
+						</li>
+					</ul>
+				</div>
+			</section>
+
+			<section class="section-faq">
+				<div class="wrapper">
+					<ul>
+						<li>
+							<a href="">
+								<div class="container-text">
+									<h3>Registration desktop </h3>
+									<p>
+										Suivez les 3 étapes de créations de compte en utilisant votre adresse mail, google ou facebook. 
+									</p>
+								</div>
+								<div class="arrow">
+									<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+										<use xlink:href="img/common/arrow-1.svg#content"></use>
+									</svg>
+								</div>
+							</a>
+						</li>
+						<li>
+							<a href="" >
+								<div class="container-text">
+									<h3>Registration desktop </h3>
+									<p>
+										Suivez les 3 étapes de créations de compte en utilisant votre adresse mail, google ou facebook. 
+									</p>
+								</div>
+								<div class="arrow">
+									<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+										<use xlink:href="img/common/arrow-1.svg#content"></use>
+									</svg>
+								</div>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<div class="container-text">
+									<h3>Registration desktop </h3>
+									<p>
+										Suivez les 3 étapes de créations de compte en utilisant votre adresse mail, google ou facebook. 
+									</p>
+								</div>
+								<div class="arrow">
+									<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+										<use xlink:href="img/common/arrow-1.svg#content"></use>
+									</svg>
+								</div>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<div class="container-text">
+									<h3>Registration desktop </h3>
+									<p>
+										Suivez les 3 étapes de créations de compte en utilisant votre adresse mail, google ou facebook. 
+									</p>
+								</div>
+								<div class="arrow">
+									<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+										<use xlink:href="img/common/arrow-1.svg#content"></use>
+									</svg>
+								</div>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<div class="container-text">
+									<h3>Registration desktop </h3>
+									<p>
+										Suivez les 3 étapes de créations de compte en utilisant votre adresse mail, google ou facebook. 
+									</p>
+								</div>
+								<div class="arrow">
+									<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+										<use xlink:href="img/common/arrow-1.svg#content"></use>
+									</svg>
+								</div>
+							</a>
+						</li>
+						<li>
+							<a href="">
+								<div class="container-text">
+									<h3>Registration desktop </h3>
+									<p>
+										Suivez les 3 étapes de créations de compte en utilisant votre adresse mail, google ou facebook. 
+									</p>
+								</div>
+								<div class="arrow">
+									<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+										<use xlink:href="img/common/arrow-1.svg#content"></use>
+									</svg>
+								</div>
+							</a>
+						</li>
+
+
+					</ul>
+					<div class="container-btn">
+						<a href="" class="btn">
+							<span class="btn-text">Charger plus </span>
+						</a>
+					</div>
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/faq-min.js"></script>
+	</body>
+</html>
