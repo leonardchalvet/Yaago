770f1b445d45cda61ba824f3fa47994f4a42c807
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-01-11 17:48:58 +0100

    Update Front -> Back

--- public/script/js/faq.js
+++ public/script/js/faq.js
@@ -1,1 +1,27 @@
 // @codekit-prepend 'common.js'
+
+$('.section-title ul li a').click(function(){
+	$('.section-title ul li a').removeClass('style-active');
+	$(this).addClass('style-active');
+
+	let data = $(this).attr('data-filter');
+	console.log(data);
+});
+
+$('.section-faq .container-btn .btn').click(function(){
+	let nb = 0;
+	$('.section-faq ul li.style-hide').each(function(){
+		if(nb < 4) {
+			$(this).removeClass('style-hide');
+		}
+		nb++;
+	});
+
+	if($('.section-faq ul li.style-hide').length == 0) {
+		$('.section-faq .container-btn .btn').addClass('style-hide');	
+	}
+});
+
+if($('.section-faq ul li.style-hide').length == 0) {
+	$('.section-faq .container-btn .btn').addClass('style-hide');	
+}
