122dec88e97c74cd69ca3ffc7b7c2cdf5dcff5d3
Léonard Chalvet <leonard@agence-me.com>
2021-01-11 15:10:34 +0100

    Change - Front - Home - English bug text cover

---
+++ Front/solution.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+
+		<title>Solution</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="style/css/solution.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/solution-min.js"></script>
+	</body>
+</html>
