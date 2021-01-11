6b11e11c6cdb6a60df537973c133261df9bafeff
Léonard Chalvet <leonard@agence-me.com>
2021-01-11 16:04:50 +0100

    Add - Front - Solution

--- Front/solution.php
+++ Front/solution.php
@@ -18,6 +18,198 @@
 		<?php include('common-header.php') ?>
 
 		<main>
+
+
+			<section class="section-cover">
+
+				<img class="illu-1" src="img/solutions/section-cover/illu-1.svg" alt="">
+				<img class="illu-2" src="img/solutions/section-cover/illu-2.svg" alt="">
+
+				<div class="wrapper">
+					<div class="container-text">
+						<h1>
+							Tout ce dont vous avez besoin au <em>même</em> endroit !
+						</h1>
+						<p>
+							Que vous soyez un particulier ou un professionnel, le succès de votre hébergement repose sur l’expérience unique que vous faîtes vivre à vos voyageurs.
+						</p>
+						<div class="container-btn">
+							<a href="" class="btn">
+								<span class="btn-text">
+									Découvrir Yaago
+								</span>
+							</a>
+							<a href="" class="btn">
+								<span class="btn-text">
+									Demander une démo
+								</span>
+							</a>
+						</div>
+					</div>
+				</div>					
+			</section>
+
+			<section class="section-solution">
+				<div class="wrapper">
+					<div class="container-title">
+						<h2>Gérez vos annonces avec Yaago</h2>
+						<p>
+							Yaago est une plateforme créée par des propriétaires français, pour aider à la gestion après-réservation de vos annonces de location courte durée. Ajouter vos différentes annonces afin de les gérer plus facilement.
+						</p>
+					</div>
+
+					<div class="container-el">
+						<div class="el">
+							<div class="illu">
+								<img src="img/solutions/section-list/illu-1.svg" alt="">
+							</div>
+							<div class="text">
+								<div class="bdg">
+									<span>Synchronisation calendrier</span>
+								</div>
+								<h3>Centralisez vos <em>annonces</em></h3>
+								<p>
+									Votre annonce est visible sur plusieurs plateformes comme Airbnb, Booking et HomeAway ?
+									Évitez le casse-tête des doubles réservations grâce à <em>la synchronisation de vos calendriers.</em> Yaago vous permet de centraliser en un seul endroit la gestion de votre planning à travers un tableau de bord unique et une vue 360° sur vos propriétés.
+								</p>
+							</div>
+						</div>
+
+						<div class="el">
+							<div class="illu">
+								<img src="img/solutions/section-list/illu-2.svg" alt="">
+							</div>
+							<div class="text">
+								<div class="bdg">
+									<span>Synchronisation calendrier</span>
+								</div>
+								<h3>Centralisez vos <em>annonces</em></h3>
+								<p>
+									Votre annonce est visible sur plusieurs plateformes comme Airbnb, Booking et HomeAway ?
+									Évitez le casse-tête des doubles réservations grâce à <em>la synchronisation de vos calendriers.</em> Yaago vous permet de centraliser en un seul endroit la gestion de votre planning à travers un tableau de bord unique et une vue 360° sur vos propriétés.
+								</p>
+							</div>
+						</div>
+
+						<div class="el">
+							<div class="illu">
+								<img src="img/solutions/section-list/illu-3.svg" alt="">
+							</div>
+							<div class="text">
+								<div class="bdg">
+									<span>Synchronisation calendrier</span>
+								</div>
+								<h3>Centralisez vos <em>annonces</em></h3>
+								<p>
+									Votre annonce est visible sur plusieurs plateformes comme Airbnb, Booking et HomeAway ?
+									Évitez le casse-tête des doubles réservations grâce à <em>la synchronisation de vos calendriers.</em> Yaago vous permet de centraliser en un seul endroit la gestion de votre planning à travers un tableau de bord unique et une vue 360° sur vos propriétés.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-why">
+				<div class="wrapper">
+					<div class="container-title">
+						<h2>Pourquoi choisir Yaago ?</h2>
+					</div>
+					<div class="container-el">
+						<div class="el">
+							<img src="img/solutions/section-why/illu-1.svg" alt="">
+							<h3>L’humain avant tout</h3>
+							<p>	
+								Vous êtes au centre de nos préoccupations. Notre priorité : vous aider à améliorer votre expérience avec des outils intuitifs.
+							</p>
+						</div>
+						<div class="el">
+							<img src="img/solutions/section-why/illu-2.svg" alt="">
+							<h3>L’humain avant tout</h3>
+							<p>	
+								Vous êtes au centre de nos préoccupations. Notre priorité : vous aider à améliorer votre expérience avec des outils intuitifs.
+							</p>
+						</div>
+						<div class="el">
+							<img src="img/solutions/section-why/illu-3.svg" alt="">
+							<h3>L’humain avant tout</h3>
+							<p>	
+								Vous êtes au centre de nos préoccupations. Notre priorité : vous aider à améliorer votre expérience avec des outils intuitifs.
+							</p>
+						</div>
+					</div>
+				</div>				
+			</section>
+
+			<section class="section-solution">
+				<div class="wrapper">
+					<div class="container-el">
+						<div class="el">
+							<div class="illu">
+								<img src="img/solutions/section-list/illu-4.svg" alt="">
+							</div>
+							<div class="text">
+								<div class="bdg">
+									<span>Synchronisation calendrier</span>
+								</div>
+								<h3>Centralisez vos <em>annonces</em></h3>
+								<p>
+									Votre annonce est visible sur plusieurs plateformes comme Airbnb, Booking et HomeAway ?
+									Évitez le casse-tête des doubles réservations grâce à <em>la synchronisation de vos calendriers.</em> Yaago vous permet de centraliser en un seul endroit la gestion de votre planning à travers un tableau de bord unique et une vue 360° sur vos propriétés.
+								</p>
+							</div>
+						</div>
+
+						<div class="el">
+							<div class="illu">
+								<img src="img/solutions/section-list/illu-5.svg" alt="">
+							</div>
+							<div class="text">
+								<div class="bdg">
+									<span>Synchronisation calendrier</span>
+								</div>
+								<h3>Centralisez vos <em>annonces</em></h3>
+								<p>
+									Votre annonce est visible sur plusieurs plateformes comme Airbnb, Booking et HomeAway ?
+									Évitez le casse-tête des doubles réservations grâce à <em>la synchronisation de vos calendriers.</em> Yaago vous permet de centraliser en un seul endroit la gestion de votre planning à travers un tableau de bord unique et une vue 360° sur vos propriétés.
+								</p>
+							</div>
+						</div>
+
+						<div class="el">
+							<div class="illu">
+								<img src="img/solutions/section-list/illu-6.svg" alt="">
+							</div>
+							<div class="text">
+								<div class="bdg">
+									<span>Synchronisation calendrier</span>
+								</div>
+								<h3>Centralisez vos <em>annonces</em></h3>
+								<p>
+									Votre annonce est visible sur plusieurs plateformes comme Airbnb, Booking et HomeAway ?
+									Évitez le casse-tête des doubles réservations grâce à <em>la synchronisation de vos calendriers.</em> Yaago vous permet de centraliser en un seul endroit la gestion de votre planning à travers un tableau de bord unique et une vue 360° sur vos propriétés.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-btn">
+				<div class="wrapper">
+					<div class="container-btn">
+						<a href="" class="btn">
+							<span class="btn-text">Découvrir Yaago</span>
+						</a>
+						<a href="" class="btn">
+							<span class="btn-text">Demander une démo</span>
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<?php include('common-section-demo.php') ?>
+
 		</main>
 
 		<?php include('common-footer.php') ?>
