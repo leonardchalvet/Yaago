afeb54089e98604d6b85cdff154db7e7c361d947
Léonard Chalvet <leonard@agence-me.com>
2021-01-11 18:08:01 +0100

    Add - Front - Partners Page

---
+++ Front/partners.php
@@ -0,0 +1,206 @@
+<html>
+	<head>
+
+		<title>Solution</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="style/css/partners.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+
+			<section class="section-cover">
+				<img class="illu-1" src="img/partners/section-cover/illu-1.svg" alt="">
+				<img class="illu-2" src="img/partners/section-cover/illu-2.svg" alt="">
+				<div class="wrapper">
+					<div class="container-title">
+						<h1>Nos partenaires</h1>
+						<p>
+							Découvrez une sélection de partenaires qualifiés et dignes de confiance, qui sauront vous accompagner pour améliorer votre qualité d’hébergement.
+						</p>
+					</div>
+					<div class="container-filter">
+						<h2>Découvrez nos partenaires et bénéficiez de nombreuses réductions.</h2>
+						<div class="filter">
+							<div class="input">
+								<input type="text" readonly placeholder="Trier par catégorie">
+								<div class="arrow">
+									<img src="img/partners/section-cover/icn-arrow.svg" alt="">
+								</div>
+							</div>
+							<div class="dropdown">
+								<div class="container-el">
+									<div class="el">
+										Option 1
+									</div>
+									<div class="el">
+										Option 1
+									</div>
+									<div class="el">
+										Option 1
+									</div>
+									<div class="el">
+										Option 1
+									</div>
+									<div class="el">
+										Option 1
+									</div>
+									<div class="el">
+										Option 1
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-partners">
+				<div class="wrapper">
+					<div class="container-el">
+						<div class="el">
+							<div class="logo">
+								<img src="img/partners/section-partners/igloohome.png" alt="">
+							</div>
+							<div class="text">
+								<h3>Serrure connectée</h3>
+								<p>
+									Avec Igloohome gérez l’ouverture de votre porte à distance. Facilitez vos check-in et votre quotidien de façon autonome.
+								</p>
+								<div class="bdg">
+									<span>
+										-30% de réduction en cliquant ci-dessous
+									</span>
+								</div>
+								<div class="btn">
+									<div class="btn-text">Profitez de la réduction</div>
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="logo">
+								<img src="img/partners/section-partners/igloohome.png" alt="">
+							</div>
+							<div class="text">
+								<h3>Serrure connectée</h3>
+								<p>
+									Avec Igloohome gérez l’ouverture de votre porte à distance. Facilitez vos check-in et votre quotidien de façon autonome.
+								</p>
+								<div class="bdg">
+									<span>
+										-30% de réduction en cliquant ci-dessous
+									</span>
+								</div>
+								<div class="btn">
+									<div class="btn-text">Profitez de la réduction</div>
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="logo">
+								<img src="img/partners/section-partners/igloohome.png" alt="">
+							</div>
+							<div class="text">
+								<h3>Serrure connectée</h3>
+								<p>
+									Avec Igloohome gérez l’ouverture de votre porte à distance. Facilitez vos check-in et votre quotidien de façon autonome.
+								</p>
+								<div class="bdg">
+									<span>
+										-30% de réduction en cliquant ci-dessous
+									</span>
+								</div>
+								<div class="btn">
+									<div class="btn-text">Profitez de la réduction</div>
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="logo">
+								<img src="img/partners/section-partners/igloohome.png" alt="">
+							</div>
+							<div class="text">
+								<h3>Serrure connectée</h3>
+								<p>
+									Avec Igloohome gérez l’ouverture de votre porte à distance. Facilitez vos check-in et votre quotidien de façon autonome.
+								</p>
+								<div class="bdg">
+									<span>
+										-30% de réduction en cliquant ci-dessous
+									</span>
+								</div>
+								<div class="btn">
+									<div class="btn-text">Profitez de la réduction</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="el">
+							<div class="logo">
+								<img src="img/partners/section-partners/igloohome.png" alt="">
+							</div>
+							<div class="text">
+								<h3>Serrure connectée</h3>
+								<p>
+									Avec Igloohome gérez l’ouverture de votre porte à distance. Facilitez vos check-in et votre quotidien de façon autonome.
+								</p>
+								<div class="soon">
+									Réduction bientôt disponible
+								</div>
+							</div>
+						</div>
+						<div class="el">
+							<div class="logo">
+								<img src="img/partners/section-partners/igloohome.png" alt="">
+							</div>
+							<div class="text">
+								<h3>Serrure connectée</h3>
+								<p>
+									Avec Igloohome gérez l’ouverture de votre porte à distance. Facilitez vos check-in et votre quotidien de façon autonome.
+								</p>
+								<div class="soon">
+									Réduction bientôt disponible
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-banner">
+				<div class="wrapper">
+					<div class="illu">
+						<img src="img/partners/section-banner/illu-1.svg" alt="">
+					</div>
+					<div class="text">
+						<h2>Un partenariat vous intéresse ?</h2>
+						<p>
+							Nous voyons en chacun de nos partenariats une relation gagnant-gagnant.  Apportez de la visibilité à votre service en le partageant à notre commaunuté.
+						</p>
+					</div>
+					<div class="btn">
+						<div class="btn-text">Devenez partenaire</div>
+					</div>
+				</div>
+			</section>
+
+			<?php include('common-section-demo.php') ?>
+
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/partners-min.js"></script>
+	</body>
+</html>
