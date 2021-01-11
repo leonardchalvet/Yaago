034ae9f0e181f37924669165bfa2b09b2a25974a
Léonard Chalvet <leonard@agence-me.com>
2021-01-11 15:03:40 +0100

    Add - Front - Blog Details

---
+++ Front/faq-details.php
@@ -0,0 +1,88 @@
+<html>
+	<head>
+
+		<title>Faq - Details</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="style/css/faq-details.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+			<section class="section-cover">
+				<div class="wrapper">
+					<div class="container-title">
+						<h1>Registration <em>desktop</em></h1>
+						<p>
+							Suivez les 3 étapes de créations de compte en utilisant votre adresse mail, google ou facebook. 
+						</p>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-details">
+				<div class="wrapper">
+					<div class="container-title">
+						<h2>
+							Lorem ipsum dolor sit amet non <em>consequentur</em>
+						</h2>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+						</p>
+					</div>
+					<div class="container-el">
+						<div class="el">
+							<div class="text">
+								<h3>1. Lorem ipsum dolor sit amet non consequentur</h3>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+									<br>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+								</p>
+							</div>
+							<img src="img/img-test/background-1.jpg" alt="">
+						</div>
+						<div class="el">
+							<div class="text">
+								<h3>1. Lorem ipsum dolor sit amet non consequentur</h3>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>
+									<br>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+								</p>
+							</div>
+							<img src="img/img-test/background-1.jpg" alt="">
+						</div>
+						<div class="el">
+							<div class="text">
+								<h3>1. Lorem ipsum dolor sit amet non consequentur</h3>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+								</p>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+								</p>
+							</div>
+							<img src="img/img-test/background-1.jpg" alt="">
+						</div>
+					</div>
+				</div>
+			</section>
+
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/faq-details-min.js"></script>
+	</body>
+</html>
