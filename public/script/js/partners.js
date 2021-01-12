efcb4ed2bba61e9eed86bf8932b2600d1634fc0f
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-01-12 14:11:32 +0100

    Update Front -> Back

---
+++ public/script/js/partners.js
@@ -0,0 +1,4 @@
+// @codekit-prepend 'common.js'
+$('.section-cover .container-filter .filter .input').click(function(){
+	$('.section-cover .container-filter .filter .dropdown').toggleClass('style-open');
+});
