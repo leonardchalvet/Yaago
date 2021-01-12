c44f3bb654246c07e88ae48e17156d9877e99d19
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-01-12 17:27:23 +0100

    Add - Back - Legal Mention - Link Prismic

---
+++ app/views/legal_mention.php
@@ -0,0 +1,56 @@
+<?php 
+use Prismic\Dom\RichText;
+$document = $WPGLOBAL['document']->data;
+?>
+<html>
+  <head>
+
+    <title><?= RichText::asText($document->global_title); ?></title>
+
+    <meta name="description" content="<?= RichText::asText($document->global_description); ?>" />
+
+    <meta property="og:image" content="<?= $document->global_image->url; ?>">
+
+    <meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="/style/css/mentions.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#03224c">
+    <meta name="msapplication-TileColor" content="#03224c">
+    <meta name="theme-color" content="#ffffff">
+    
+  </head>
+  
+  <body>
+
+    <?php include('common-header.php') ?>
+
+    <main>
+
+     <section class="section-cover">
+        <div class="wrapper">
+          <div class="container-text">
+            <?= RichText::asHtml($document->title); ?>
+          </div>
+        </div>
+      </section>
+
+      <section class="section-mentions">
+        <div class="wrapper">
+          <?= RichText::asHtml($document->content); ?>
+        </div>
+      </section>
+      
+    </main>
+
+    <?php include('common-footer.php') ?>
+
+    <script type="text/javascript" src="/script/minify/mentions-min.js"></script>
+  </body>
+</html>
