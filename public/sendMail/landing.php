6b24050096c539d3ff2446022898957b55d05169
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-01-13 10:48:56 +0100

    Add - Back - Landing - send mail script

---
+++ public/sendMail/landing.php
@@ -0,0 +1,13 @@
+<?php
+
+$email    = isset($_POST['email'])   ?  $_POST['email']   : null ;
+$sendto   = isset($_POST['sendto'])  ?  $_POST['sendto']  : null ;
+$object   = isset($_POST['object'])  ?  $_POST['object']  : null ;
+
+if($email != null && $sendto != null && $object != null ) {
+	$header = "From: ".$email." \n";
+
+	$content =  "Email : "    . "\n" . $email    . "\n" . "\n";
+
+	mail($sendto, $object, $content, $header);
+}
