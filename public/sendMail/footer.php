9cd10873a3489b5489926208701d6a4333426f4f
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-01-13 10:48:26 +0100

    Change - Back - Footer - Fix redirection

--- public/sendMail/footer.php
+++ public/sendMail/footer.php
@@ -3,14 +3,11 @@
 $email    = isset($_POST['email'])   ?  $_POST['email']   : null ;
 $sendto   = isset($_POST['sendto'])  ?  $_POST['sendto']  : null ;
 $object   = isset($_POST['object'])  ?  $_POST['object']  : null ;
-$goto     = isset($_POST['goto'])    ?  $_POST['goto']    : null ;
 
-if($email != null && $sendto != null && $object != null && $goto != null ) {
+if($email != null && $sendto != null && $object != null ) {
 	$header = "From: ".$email." \n";
 
 	$content =  "Email : "    . "\n" . $email    . "\n" . "\n";
 
 	mail($sendto, $object, $content, $header);
 }
-
-header('Location: ' .$goto);
