69d09e4c5f5d9e29cefcc511582e9c8ca72ab31c
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-01-13 10:47:05 +0100

    Update Front -> Back

--- public/script/js/landing.js
+++ public/script/js/landing.js
@@ -1,1 +1,40 @@
 // @codekit-prepend 'common.js'
+
+$('.section-cover form button').on('click', function() {
+
+	let returnF = true;
+	$(this).parent().parent().find('input').each(function(){
+		
+		if( isEmpty($(this)) ) {
+			returnF = false;
+			$(this).parent().parent().addClass('style-error');
+		}
+		else {
+			$(this).parent().parent().removeClass('style-error');
+		}
+
+		if($(this).attr("name") == 'email') {
+			let returnV = verifEmail($(this));
+			if(!returnV) {
+				returnF = false;
+				$(this).parent().parent().addClass('style-error');
+			}
+		}
+	})
+
+	if(returnF) {
+		let form = $(this).parent().parent();
+		$.ajax({
+			url : '/sendMail/landing.php',
+			type : 'POST',
+			data : form.serialize(),
+			success : function(code, statut){
+				$('.section-cover .container-send').addClass('style-active');
+				setTimeout(function(){
+					$('.section-cover .container-send').removeClass('style-active');
+					window.location.href = $('.section-cover .container-title .container-input .input input[name=goto]').val();
+				}, 3000);
+			}
+		});
+	}
+})
