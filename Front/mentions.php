a341b5b1da13275c3735362729c77b7866ddf851
Alexis Prevost-Maynen <killer76520@gmail.com>
2021-02-04 17:25:41 +0100

    Change - Front - Mentions - Update responsive & JS

--- Front/mentions.php
+++ Front/mentions.php
@@ -27,127 +27,115 @@
 							Conditions <em>d’utilisation</em>
 						</h1>
 					</div>
-					
 				</div>
 				<div class="container-filter">
 					<ul>
 						<li>
-							<a class="style-active" data-filter=".">
-								Mentions légales 
-							</a>
+							<a class="style-active">Mentions légales</a>
 						</li>
 						<li>
-							<a data-filter="Login / Register">
-								Charte des cookies
-							</a>
+							<a>Charte des cookies</a>
 						</li>
 						<li>
-							<a data-filter="Recommendations">	
-								Conditions générales d’utilisation
-							</a>
+							<a>Conditions générales d’utilisation</a>
 						</li>
 						<li>
-							<a data-filter="General / Settings">
-								Conditions générales de ventes 
-							</a>
+							<a>Conditions générales de ventes</a>
 						</li>
 					</ul>
 				</div>
 			</section>
 
-			<section class="section-mentions">
-				<div class="wrapper">
-					<h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
-					<h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h3>
-					<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
-					<p>Lorem ipsum dolor <a href="">sit amet</a> consectetur adipisicing elit. </p>
-					<ol>
-						<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
-						<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
-						<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
-					</ol>
-					<ul>
-						<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
-						<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
-						<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
-					</ul>
-
-
-					<div class="container-el">
-						<div class="el">
-							<div class="head">
-								<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
-								<div class="arrow">
-									<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+			<div class="container-section">
+				<?php for($i=0;$i<4;$i++) { ?>
+				<section class="section-mentions <?php if($i==0) echo('style-active'); ?>">
+					<div class="wrapper">
+						<h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h2>
+						<h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h3>
+						<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
+						<p>Lorem ipsum dolor <a href="">sit amet</a> consectetur adipisicing elit. </p>
+						<ol>
+							<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
+							<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
+							<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
+						</ol>
+						<ul>
+							<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
+							<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
+							<li>Lorem ipsum dolor sit amet consectetur adipisicing elit. </li>
+						</ul>
+						<div class="container-el">
+							<div class="el">
+								<div class="head">
+									<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
+									<div class="arrow">
+										<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+									</div>
 								</div>
+								<p>
+									Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
+								</p>
 							</div>
-							<p>
-								Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
-							</p>
-						</div>
-
-						<div class="el">
-							<div class="head">
-								<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
-								<div class="arrow">
-									<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+							<div class="el">
+								<div class="head">
+									<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
+									<div class="arrow">
+										<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+									</div>
 								</div>
+								<p>
+									Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
+								</p>
 							</div>
-							<p>
-								Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
-							</p>
-						</div>
-
-						<div class="el">
-							<div class="head">
-								<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
-								<div class="arrow">
-									<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+							<div class="el">
+								<div class="head">
+									<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
+									<div class="arrow">
+										<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+									</div>
 								</div>
+								<p>
+									Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
+								</p>
 							</div>
-							<p>
-								Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
-							</p>
-						</div>
-
-						<div class="el">
-							<div class="head">
-								<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
-								<div class="arrow">
-									<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+							<div class="el">
+								<div class="head">
+									<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
+									<div class="arrow">
+										<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+									</div>
 								</div>
+								<p>
+									Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
+								</p>
 							</div>
-							<p>
-								Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
-							</p>
-						</div>
-
-						<div class="el">
-							<div class="head">
-								<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
-								<div class="arrow">
-									<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+							<div class="el">
+								<div class="head">
+									<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
+									<div class="arrow">
+										<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+									</div>
 								</div>
+								<p>
+									Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
+								</p>
 							</div>
-							<p>
-								Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
-							</p>
-						</div>
-
-						<div class="el">
-							<div class="head">
-								<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
-								<div class="arrow">
-									<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+							<div class="el">
+								<div class="head">
+									<h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h4>
+									<div class="arrow">
+										<img src="img/offre/section-faq/icn-arrow.svg" alt="">
+									</div>
 								</div>
+								<p>
+									Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
+								</p>
 							</div>
-							<p>
-								Cette page (ainsi que les documents auxquels elle renvoie) vous indique les conditions d'utilisation de notre site https://yaago.com/ et de ses sous-domaines ("site web"), <a href="">sit amet</a> que vous soyez un invité ou un utilisateur enregistré. Veuillez lire attentivement ces conditions d'utilisation avant de commencer à utiliser le site web. En utilisant notre site web, vous indiquez que vous acceptez les présentes conditions d'utilisation du site web et que vous vous engagez à les respecter. Si vous n'acceptez pas ces conditions d'utilisation du site, veuillez vous abstenir d'utiliser notre site.
-							</p>
 						</div>
 					</div>
-				</div>
-			</section>
+				</section>
+				<?php } ?>
+			</div>
 
 
 		</main>
--- Front/script/js/mentions.js
+++ Front/script/js/mentions.js
@@ -1,12 +1,20 @@
 // @codekit-prepend 'common.js'
 
 $('.section-mentions .container-el .el .head').click(function(){
-	
-
 	if ($(this).closest('.el').hasClass('style-active')) {
-		
 		$(this).closest('.el').removeClass('style-active');
 	} else {
 		$(this).closest('.el').addClass('style-active');
 	}
-})
+});
+
+$('.section-cover .container-filter ul li a').click(function(){
+	if(!$(this).hasClass('style-active')) {
+		$('.section-cover .container-filter ul li a').removeClass('style-active');
+		$(this).addClass('style-active');
+
+		let index = $(this).parent().index() + 1;
+		$('.container-section .section-mentions').removeClass('style-active');
+		$('.container-section .section-mentions:nth-child(' + index + ')').addClass('style-active');
+	}
+});
