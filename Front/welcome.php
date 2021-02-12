898037ab5277cc9aeb6f6ccc55a65c5a72928e7b
Léonard Chalvet <leonard@agence-me.com>
2021-02-12 09:05:51 +0100

    Change - Front - Add - Welcome

---
+++ Front/welcome.php
@@ -0,0 +1,120 @@
+<html>
+	<head>
+
+		<title>Solution</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+		<link rel="stylesheet" type="text/css" href="style/css/welcome.css">
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+
+
+			<section class="section-features">
+				<div class="wrapper">
+					<div class="container-features">
+						<div class="container-text">
+							<h1>
+								Commencez l’aventure <br>en profitant <br>de 3 mois <em>offerts</em>
+							</h1>
+							<a href="" class="btn">
+								<span class="btn-text">Inscrivez-vous dès maintenant</span>
+								<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+									<use xlink:href="img/common/arrow-1.svg#content"></use>
+								</svg>
+							</a>
+						</div>
+						<div class="slider-features">
+							<div class="slider">
+								<div class="feature">
+									<h2>Centralisez<br> vos annonces</h2>
+									<img src="img/welcome/illu-1.png" alt="">
+								</div>
+								<div class="feature">
+									<h2>Communiquez<br> avec vos coéquipiers</h2>
+									<img src="img/welcome/illu-2.png" alt="">
+								</div>
+								<div class="feature">
+									<h2>Recommandez<br> vos bonnes adresses</h2>
+									<img src="img/welcome/illu-3.png" alt="">
+								</div>
+								<div class="feature">
+									<h2>Consultez<br> vos statistiques</h2>
+									<img src="img/welcome/illu-4.png" alt="">
+								</div>
+								<div class="feature">
+									<h2>Découvrez les<br> messages automatiques</h2>
+									<img src="img/welcome/illu-5.png" alt="">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="container-btn">
+						<a href="" class="btn">
+							<span class="btn-text">En savoir plus</span>
+						</a>
+					</div>
+				</div>
+			</section>
+
+			<section class="section-tarifs">
+				<div class="wrapper">
+
+					<div class="content">
+						<div class="container-text">
+							<h3>Enfin une tarification simple et transparente</h3>
+							<p class="desc">
+								Nous vous proposons un tarif dégressif, sans frais d’installation et sans formation payante.
+							</p>
+							<p class="as">
+								Gratuit pendant les 3 premiers mois (pas besoin de CB) Aucun frais d’installation et sans engagement.
+							</p>
+						</div>
+
+						<div class="container-tarifs">
+							<img class="illu-obj" src="img/welcome/illu-obj.svg" alt="">
+							<div class="banner">
+								<span>Après les 3 mois offerts</span>
+							</div>
+							<div class="container-el">
+								<div class="el">
+									<div class="price">24,90€</div>
+									<div class="subtitle">/mois</div>
+									<div class="text">Pour les 2 premières propriétés</div>
+								</div>
+								<div class="el">
+									<div class="price">24,90€</div>
+									<div class="subtitle">/mois /propriété</div>
+									<div class="text">Dès la 3ème propriété</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					<div class="container-btn">
+						<a href="" class="btn">
+							<span class="btn-text">Testez</span>
+							<svg class="btn-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 12" >
+								<use xlink:href="img/common/arrow-1.svg#content"></use>
+							</svg>
+						</a>
+					</div>
+				</div>
+			</section>
+
+
+		</main>
+
+		<script type="text/javascript" src="script/minify/welcome-min.js"></script>
+	</body>
+</html>
