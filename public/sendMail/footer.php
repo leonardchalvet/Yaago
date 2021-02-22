5ed8eca97d94fa743ecfc41fe201e31bdf868b87
David <david@infine.io>
2021-02-22 19:19:39 +0100

    Try to remove Email: when sending emails

--- public/sendMail/footer.php
+++ public/sendMail/footer.php
@@ -7,7 +7,7 @@ $object   = isset($_POST['object'])  ?  $_POST['object']  : null ;
 if($email != null && $sendto != null && $object != null ) {
 	$header = "From: ".$email." \n";
 
-	$content =  "Email : "    . "\n" . $email    . "\n" . "\n";
+	$content =  $email;
 
 	mail($sendto, $object, $content, $header);
 }
