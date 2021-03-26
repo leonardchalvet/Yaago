4ecd44c31034d50495020770aa0c7fd2abd51d6c
Alexis Prevost-Maynen <alexis@callbruno.com>
2021-03-26 11:08:27 +0100

    Change - Back - Common Footer - Update script

--- app/views/common-footer.php
+++ app/views/common-footer.php
@@ -116,11 +116,11 @@ $url = $urlt[0].'//'.$urlt[2].'/';
 
 
 <!-- ActiveCampaign Code -->
-<script type=“text/javascript”>
-    (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement(“script”);r.src=o;r.async=true;i=t.getElementsByTagName(“script”)[0];i.parentNode.insertBefore(r,i)})(window,document,“https://diffuser-cdn.app-us1.com/diffuser/diffuser.js”,“vgo”);
-    vgo(‘setAccount’, ‘649935505’);
-    vgo(‘setTrackByDefault’, true);
-    vgo(‘process’);
+<script type="text/javascript">
+    (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+    vgo('setAccount', '649935505');
+    vgo('setTrackByDefault', true);
+    vgo('process');
 </script>
 <!-- End ActiveCampaign Code -->
 
