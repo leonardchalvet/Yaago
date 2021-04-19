1cad38bc58610873cebcb0bad40eab9f9ea105bb
ed-infine-io <edouard@infine.io>
2021-04-19 22:14:54 +0200

    add active campaign on livre blanc download

--- public/sendMail/home.php
+++ public/sendMail/home.php
@@ -8,12 +8,23 @@ $sendto    = isset($_POST['sendto'])     ?  $_POST['sendto']    : null ;
 $object    = isset($_POST['object'])     ?  $_POST['object']    : null ;
 
 if($firstname != null && $lastname != null && $email != null && $phone != null && $sendto != null && $object != null ) {
+	/*
 	$header = "From: ".$object." \n";
-
 	$content =  "Firstname : " . "\n" . $firstname . "\n" . "\n".
 				"Lastname : "  . "\n" . $lastname  . "\n" . "\n".
 				"Email : "     . "\n" . $email     . "\n" . "\n".
 				"Phone : "     . "\n" . $phone     . "\n" . "\n";
-
 	mail($sendto, $object, $content, $header);
+	*/
+	require_once("includes/ActiveCampaign.class.php");
+	$ac = new ActiveCampaign("https://infine.api-us1.com", "7a7919c41e38f8b803424bcf45b7262855f74bca27ec9be62e232ad2d3784ffe5877264a");
+	$contact = array(
+		"email"              => $email,
+		"first_name"         => $firstname,
+		"last_name"          => $lastname,
+		"tags"      => "livre-blanc"
+	);
+
+	$contact_sync = $ac->api("contact/sync", $contact);
+
 }
